43ad74e6b1a8a3940b8f00e3f1c7e2f656db19a8
shunkin <shunkin@sfc.wide.ad.jp>
2015-01-27 13:32:18 +0900

    added button and http request.

--- app/src/main/java/com/example/kinoshitashun/kosmosclient/MainActivity.java
+++ app/src/main/java/com/example/kinoshitashun/kosmosclient/MainActivity.java
@@ -1,30 +1,49 @@
 package com.example.kinoshitashun.kosmosclient;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends ActionBarActivity {
+    public static boolean isConnectedToInternet;
+    public static Context contextVar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        isConnectedToInternet = isConnected();
+        contextVar = getBaseContext();
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -47,20 +66,94 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
+    public boolean isConnected(){
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Activity.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected())
+            return true;
+        else
+            return false;
+    }
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
-
+    public static class PlaceholderFragment extends Fragment implements View.OnClickListener {
+        TextView textArea;
+        TextView resultView;
         public PlaceholderFragment() {
         }
-
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            textArea = (TextView) rootView.findViewById(R.id.textArea);
+            resultView = (TextView) rootView.findViewById(R.id.resultView);
+            Button btn = (Button) rootView.findViewById(R.id.button);
+            btn.setOnClickListener(this);
             return rootView;
+        }
+        private class HttpAsyncTask extends AsyncTask<String, Void, String> {
+            @Override
+            protected String doInBackground(String... urls) {
+
+                return GET(urls[0]);
+            }
+            // onPostExecute displays the results of the AsyncTask.
+            @Override
+            protected void onPostExecute(String result) {
+                Toast.makeText(contextVar, "Received!", Toast.LENGTH_LONG).show();
+                resultView.setText(result);
+            }
+        }
+        public static String GET(String url){
+            InputStream inputStream = null;
+            String result = "";
+            try {
+
+                // create HttpClient
+                HttpClient httpclient = new DefaultHttpClient();
+
+                // make GET request to the given URL
+                HttpResponse httpResponse = httpclient.execute(new HttpGet(url));
+
+                // receive response as inputStream
+                inputStream = httpResponse.getEntity().getContent();
+
+                // convert inputstream to string
+                if(inputStream != null)
+                    result = convertInputStreamToString(inputStream);
+                else
+                    result = "Did not work!";
+
+            } catch (Exception e) {
+                Log.d("InputStream", e.getLocalizedMessage());
+            }
+
+            return result;
+        }
+
+        private static String convertInputStreamToString(InputStream inputStream) throws IOException {
+            BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+            String line = "";
+            String result = "";
+            while((line = bufferedReader.readLine()) != null)
+                result += line;
+
+            inputStream.close();
+            return result;
+
+        }
+
+        @Override
+        public void onClick(View rootView){
+            Log.d("debug msg","clicked");
+            if(isConnectedToInternet) {
+                textArea.setBackgroundColor(0xFF00CC00);
+                textArea.setText("You are connected to Internet");
+                new HttpAsyncTask().execute("http://hmkcode.appspot.com/rest/controller/get.json");
+            }else {
+                textArea.setText("You are not connected to Internet");
+            }
         }
     }
 }
